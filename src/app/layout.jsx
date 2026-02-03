df180b7d9f9987ef2029bc1c356afe54d4a8c1ba
Kunal Bhatia <kunal261708@gmail.com>
2026-02-03 20:46:39 +0530

    set light as default theme

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -65,7 +65,7 @@ export default function RootLayout({ children }) {
         <ToastContainer />
         <ThemeProvider
           attribute="class"
-          defaultTheme="dark"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
